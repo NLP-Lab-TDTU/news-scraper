0b7c5612c9c998f8148e5d3adbba7daac2285086
Hieu Nguyen <hieunguyen1053@outlook.com>
2023-04-13 15:06:58 +0700

    reformat tuoitre

--- news/news/spiders/tuoitre.py
+++ news/news/spiders/tuoitre.py
@@ -1,7 +1,10 @@
+import re
+
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
 from ..items import NewsItem
+
 
 class TuoitreSpider(CrawlSpider):
     name = "tuoitre"
@@ -25,6 +28,7 @@ class TuoitreSpider(CrawlSpider):
         p_tags = content_tag.css('p:not(.VCObjectBoxRelatedNewsItemSapo)')
         p_texts = [p.css('::text').extract_first() for p in p_tags]
         item['content'] = '\n\n'.join([p.strip() for p in p_texts if p is not None])
+        item['content'] = re.sub(r'\n{3,}', '\n\n', item['content'])
 
         date = response.css('div.detail-time div[data-role="publishdate"]::text').extract_first().strip()
         item['extra_metadata'] = {'date': date}
