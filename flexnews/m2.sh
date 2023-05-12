6b20978f9742c6b68c9ff4f3fdf30771b75800f5
h4438 <www.hao2912@gmail.com>
2023-05-12 16:00:17 +0700

    fixed: return carriage

--- flexnews/m2.sh
+++ flexnews/m2.sh
@@ -1,9 +1,11 @@
-file='check.txt'
+file='links/lkad'
 while IFS=',' read -r DOMAIN URL; do
-	mkdir ./$DOMAIN
-	timeout 300 bash -c "FOLDER="$DOMAIN" scrapy crawl flexnews -a domain="$DOMAIN" -a url="$URL
-	wc=$(ls ./$DOMAIN | wc -w)
-	request='http://localhost:8080/feedback?folder='$DOMAIN'&url='$URL'&wc='$wc
+	mkdir ./out/$DOMAIN
+	URL=$(echo $URL | tr -d '\r')
+	DOMAIN=$(echo $DOMAIN | tr -d '\r')
+	timeout 180 bash -c "FOLDER="./out/$DOMAIN" scrapy crawl flexnews -a domain="$DOMAIN" -a url="$URL
+	wc=$(ls out/$DOMAIN | wc -w)
+	request='http://localhost:8081/feedback?folder='$DOMAIN'&url='$URL'&wc='$wc
 	echo $request
 	curl -X GET $request
 done <$file
